63f1e41614ad8be94de50d831ed66edb2b134be4
hiroto <tohhiro@gmail.com>
2025-10-04 15:30:28 +0900

    記事検索ページを追加

--- app/routes.ts
+++ app/routes.ts
@@ -3,4 +3,5 @@ import { type RouteConfig, index, route } from "@react-router/dev/routes";
 export default [
   index("routes/home.tsx"),
   route("popular", "routes/popular.tsx"),
+  route("search", "routes/search.tsx"),
 ] satisfies RouteConfig;
---
+++ app/routes/search.tsx
@@ -0,0 +1,7 @@
+export default function Search() {
+  return (
+    <div className="flex sm:ml-64">
+      <h1>記事検索</h1>
+    </div>
+  );
+}
