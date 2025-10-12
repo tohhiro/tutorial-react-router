280adea6eaa254f6665555d5b1abc74a82daaa31
hiroto <tohhiro@gmail.com>
2025-10-12 11:08:55 +0900

    fetherをカスタムhooksにし、記事検索ページでも使うように変更

--- app/routes/search.tsx
+++ app/routes/search.tsx
@@ -1,7 +1,27 @@
+import { type ArticleData, useFetcher } from "~/hooks/useFetcher";
+import { useLoaderData } from "react-router";
+
+export async function loader() {
+  return await useFetcher();
+}
+
 export default function Search() {
+  const { articles } = useLoaderData<typeof loader>();
+
   return (
-    <div className="flex sm:ml-56">
-      <h1>記事検索</h1>
+    <div className="sm:ml-56">
+      <h1>検索ページ</h1>
+
+      <ul className="list-disc list-outside">
+        {articles.map((article: ArticleData) => (
+          <li key={article.id}>{`${article.id}: ${article.title}`}</li>
+        ))}
+      </ul>
+
+      {/* <fetcher.Form method="post">
+        <input type="text" name="keyword" />
+        <button type="submit">検索</button>
+      </fetcher.Form> */}
     </div>
   );
 }
