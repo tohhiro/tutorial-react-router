96a98fc51b43c0e9ab5b90a10da30f03eff77c22
hiroto <tohhiro@gmail.com>
2025-10-22 21:52:53 +0900

    `useFetchPopular`のUTを追加 - hooksの返り値のキー名を他と統一するよう変更

--- app/feature/popular/hooks/useFetchPopular.ts
+++ app/feature/popular/hooks/useFetchPopular.ts
@@ -4,6 +4,6 @@ export const useFetchPopular = async () => {
   const res = await fetch(
     "https://jsonplaceholder.typicode.com/posts?id=2&id=10"
   );
-  const posts: ArticleData[] = await res.json();
-  return { posts };
+  const articles: ArticleData[] = await res.json();
+  return { articles };
 };
